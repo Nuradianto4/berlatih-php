e08bb2758ed756b7dc7d981ab99a187f1f271e10
Nuradianto4 <adhisopo44@gmail.com>
2020-07-30 00:33:17 +0700

    mencoba function

--- papan-catur.php
+++ papan-catur.php
@@ -2,10 +2,24 @@
 
 function papan_catur($angka) {
 // tulis kode di sini
+	for ($x=0; $x < $angka; $x++) { 
+		for ($y=0; $y < ($angka); $y++) { 
+			if ($y%2==1 && $x%2==1) {
+				echo "bener";
+			}
+			elseif ($y%2==0 && $x%2 == 0) {
+				echo "bener";
+			}
+			else{
+				echo "salah";
+			}
+		}
+		echo "<br>";
+	}
 }
 
 // TEST CASES
-echo papan_catur(4) 
+echo papan_catur(4); 
 /*
 # # # #
  # # #
@@ -13,7 +27,7 @@ echo papan_catur(4)
  # # #
  */
 
-echo papan_catur(8)
+echo papan_catur(8);
 /* 
 # # # # # # # #
  # # # # # # # 
