53fbe52e815c25506963d338517c82862b7c8d2c
Nuradianto4 <adhisopo44@gmail.com>
2020-07-30 00:10:46 +0700

    membuat function palingdrome untuk keterangan true/false

--- palingdrome-angka.php
+++ palingdrome-angka.php
@@ -1,11 +1,19 @@
 <?php
 
+function palingdrome($str){
+	$output="false";
+	if(strrev($str)==$str){
+		$output="false";
+	}
+	return $output;
+}
 function palindrome_angka($angka) {
   // tulis kode di sini
+	
 }
 
 // TEST CASES
-echo palindrome_angka(8) // 9
+echo palindrome_angka(8); // 9
 echo palindrome_angka(10); // 11
 echo palindrome_angka(117); // 121
 echo palindrome_angka(175); // 181
