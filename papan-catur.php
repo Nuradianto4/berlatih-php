f664aeeb1342efc235ebee99f265f7499c27f52e
Nuradianto4 <adhisopo44@gmail.com>
2020-07-30 00:49:38 +0700

    testing

--- papan-catur.php
+++ papan-catur.php
@@ -3,15 +3,16 @@
 function papan_catur($angka) {
 // tulis kode di sini
 	for ($x=0; $x < $angka; $x++) { 
-		for ($y=0; $y < ($angka); $y++) { 
+		for ($y=0; $y < ($angka*2)-1; $y++) { 
 			if ($y%2==1 && $x%2==1) {
-				echo "bener";
+				echo " #";
 			}
 			elseif ($y%2==0 && $x%2 == 0) {
-				echo "bener";
+				echo " g";
 			}
 			else{
-				echo "salah";
+				echo " ";
+				echo " ";
 			}
 		}
 		echo "<br>";
@@ -19,7 +20,7 @@ function papan_catur($angka) {
 }
 
 // TEST CASES
-echo papan_catur(4); 
+echo papan_catur(4)."<br> <br>"; 
 /*
 # # # #
  # # #
@@ -27,7 +28,7 @@ echo papan_catur(4);
  # # #
  */
 
-echo papan_catur(8);
+echo papan_catur(8)."<br> <br>";
 /* 
 # # # # # # # #
  # # # # # # # 
@@ -37,8 +38,8 @@ echo papan_catur(8);
  # # # # # # #
 # # # # # # # #
  # # # # # # #
-
-echo papan_catur(5) 
+*/
+echo papan_catur(5); 
 /*
 # # # # #
  # # # #
